5aadbf52cdb50fbc470581e25738ff40c7d5a07b
Tien <quangtien05022001@gmail.com>
2023-12-05 21:56:59 +0700

    Done

---
+++ src/HomeWork/Problem2/StaticMethod.java
@@ -0,0 +1,18 @@
+package HomeWork.Problem2;
+
+public class StaticMethod {
+    private static final double PI=3.14F;
+    public static double calCircleArea(double radius){
+        return radius*radius*PI;
+    }
+
+    public static double calRectangleArea(double height,double width){
+        return height*width;
+    }
+
+    public static double calTriangleArea(double a,double b,double c){
+        double s = (a + b + c) / 2;
+        return Math.sqrt(s * (s - a) * (s - b) * (s - c));
+
+    }
+}
